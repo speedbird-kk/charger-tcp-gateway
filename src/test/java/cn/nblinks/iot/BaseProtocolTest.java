474f0aab89b70019fd3a5c0b55801a262a60acba
Kanta Kiyomoto <k.kiyomoto@student.tue.nl>
2026-07-27 18:03:14 +0900

    added JUnit 4 as dependency, created BaseProtocolTest and wrote a test that PASSES for encodeSnd0x08StartProducesGoldenFrame

---
+++ src/test/java/cn/nblinks/iot/BaseProtocolTest.java
@@ -0,0 +1,37 @@
+package cn.nblinks.iot;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import cn.nblinks.iot.dataform.SendMessageForm;
+import cn.nblinks.iot.dataform.encode.Snd0x08EncodeForm;
+import cn.nblinks.teask.gateway.DeviceRegistry;
+
+public class BaseProtocolTest {
+    private static final int DEVICE_ID = 10000064;
+    private static final String GOLDEN_FRAME_SND_0x08_START = "FE001400010008000000019F1BDDD164030001F40092D3";
+
+    @Test
+    public void encodeSnd0x08StartProducesGoldenFrame() {
+        Snd0x08EncodeForm cmd = new Snd0x08EncodeForm(
+            DEVICE_ID, // deviceId
+            null, // mainDeviceId
+            DeviceRegistry.get().nextSerial(DEVICE_ID), // serialNo
+            3, // slotNo
+            0, // chargeType
+            GOLDEN_FRAME_SND_0x08_START.substring(14, 32), // flowNo
+            500, // money
+            null, // chargeScheme
+            "", // cardNo
+            0, // count
+            0 // cardBalance
+        );
+
+        SendMessageForm sendForm = new SendMessageForm(cmd);
+
+        String actual = BaseProtocol.enCode(sendForm);
+
+        assertEquals(GOLDEN_FRAME_SND_0x08_START, actual);
+    }
+}
