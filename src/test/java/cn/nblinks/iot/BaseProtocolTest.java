56ffae5a13835b48c61dfaa3d9835fc42df0a204
Kanta Kiyomoto <k.kiyomoto@student.tue.nl>
2026-07-28 20:15:14 +0900

    Test for encoding REC_0x01_LOGIN producing the golden frame written and passes, data is build from manually decoded golden frame fields same as buildLoginData() in the ChargerSimulator while softwareVersion hex is padded to 26 bytes within the test method.

--- src/test/java/cn/nblinks/iot/BaseProtocolTest.java
+++ src/test/java/cn/nblinks/iot/BaseProtocolTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import cn.nblinks.iot.constants.CMD;
 import cn.nblinks.iot.constants.PROTOCOLS;
 import cn.nblinks.iot.dataform.SendMessageForm;
 import cn.nblinks.iot.dataform.encode.Snd0x08EncodeForm;
@@ -14,7 +15,41 @@ import cn.nblinks.teask.gateway.DeviceRegistry;
 
 public class BaseProtocolTest {
     private static final int DEVICE_ID = 10000064;
+    private static final String GOLDEN_FRAME_REC_0x01_LOGIN 
+        = "FE002E0001000100100000640A0100544541534b2d53494d0000000000000000000000000000000000000000000DAC8090";
     private static final String GOLDEN_FRAME_SND_0x08_START = "FE001400010008000000019F1BDDD164030001F40092D3";
+
+    @Test
+    public void encodeRec0x01LoginProducesGoldenFrame() {
+        StringBuilder softwareVersion = new StringBuilder(
+            ByteUtil.convertStringToHex("TEASK-SIM")
+        );
+
+        while (softwareVersion.length() < 52) {
+            softwareVersion.append("0");
+        }
+    
+        StringBuilder data = new StringBuilder();
+        data.append(String.format("%010d", DEVICE_ID)); // Device id (decimals)
+        data.append(ByteUtil.decimal2fitHex(10, 2)); // Slot count
+        data.append(ByteUtil.decimal2fitHex(1, 2)); // Transmission version
+        data.append(ByteUtil.decimal2fitHex(0, 2)); // Device type
+        data.append(softwareVersion.toString()); // Software version
+        data.append("00000000"); // Function mask
+        data.append(ByteUtil.decimal2fitHex(3500, 4)); // Slot max power (W)
+
+        SendMessageForm sendForm = new SendMessageForm();
+
+        sendForm.setDeviceId(DEVICE_ID);
+        sendForm.setSubDeviceId(null);
+        sendForm.setSerialNo(1);
+        sendForm.setCmd(CMD.REC_0x01_LOGIN);
+        sendForm.setData(data.toString());
+
+        String actual = BaseProtocol.enCode(sendForm);
+
+        assertEquals(GOLDEN_FRAME_REC_0x01_LOGIN, actual);
+    }
 
     @Test
     public void encodeSnd0x08StartProducesGoldenFrame() {
