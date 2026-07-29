e6f3dd499784e7ff7d68b4e0f50102558263a5ae
Kanta Kiyomoto <k.kiyomoto@student.tue.nl>
2026-07-30 01:11:44 +0900

    Tests written and PASSED for encoding Snd0x02 and Rec0x11 producing golden frames. Note that for Snd0x02, the encode form constructor sets the timestamp to the current time / 1000 at time of running the test, so for the test to pass it is necessary to mutate the timestamp using a setter for the timestamp to set it to the timestamp as is on the golden frame.

--- src/test/java/cn/nblinks/iot/BaseProtocolTest.java
+++ src/test/java/cn/nblinks/iot/BaseProtocolTest.java
@@ -10,17 +10,26 @@ import cn.nblinks.iot.constants.CMD;
 import cn.nblinks.iot.constants.PROTOCOLS;
 import cn.nblinks.iot.dataform.BaseMessageForm;
 import cn.nblinks.iot.dataform.SendMessageForm;
+import cn.nblinks.iot.dataform.encode.Snd0x02EncodeForm;
 import cn.nblinks.iot.dataform.encode.Snd0x08EncodeForm;
 import cn.nblinks.iot.utils.ByteUtil;
-import cn.nblinks.teask.gateway.DeviceRegistry;
 
 public class BaseProtocolTest {
     private static final int DEVICE_ID = 10000064;
     private static final String GOLDEN_FRAME_REC_0x01_LOGIN 
         = "FE002E0001000100100000640A0100544541534b2d53494d0000000000000000000000000000000000000000000DAC8090";
+
     private static final String GOLDEN_FRAME_REC_0x0D_START_REP
         = "FE001A0002000D000000019F1BDDD164030001F46553F10000000088BD";
-    private static final String GOLDEN_FRAME_SND_0x08_START = "FE001400010008000000019F1BDDD164030001F40092D3";
+
+    private static final String GOLDEN_FRAME_REC_0x11_ORDER_END
+        = "FE002800030011000000019F1BDDD16403000000006553F1006553F13C0001003201F401F4012C0000A671";
+
+    private static final String GOLDEN_FRAME_SND_0X02_LOGIN_REP
+        = "FE000B00010002006A4493248609";
+
+    private static final String GOLDEN_FRAME_SND_0x08_START
+        = "FE001400010008000000019F1BDDD164030001F40092D3";
 
     @Test
     public void encodeRec0x01LoginProducesGoldenFrame() {
@@ -76,6 +85,60 @@ public class BaseProtocolTest {
         String actual = BaseProtocol.enCode(sendForm);
 
         assertEquals(GOLDEN_FRAME_REC_0x0D_START_REP, actual);
+    }
+
+    @Test
+    public void encodeRec0x11OrderEndProducesGoldenFrame() {
+        StringBuilder data = new StringBuilder();
+        data.append(GOLDEN_FRAME_REC_0x11_ORDER_END.substring(14, 32)); // Flow number
+        data.append(ByteUtil.decimal2fitHex(3, 2)); // Slot number
+        data.append(ByteUtil.decimal2fitHex(0, 2)); // Charge type
+        data.append(ByteUtil.decimal2fitHex(0, 2)); // Reason
+        data.append(ByteUtil.decimal2fitHex(0, 4)); // Error code
+        data.append("6553F100"); // Start time
+        data.append("6553F13C"); // End time
+        data.append(ByteUtil.decimal2fitHex(1, 4)); // Time
+        data.append(ByteUtil.decimal2fitHex(50, 4)); // Electricity
+        data.append(ByteUtil.decimal2fitHex(500, 4)); // Total money
+        data.append(ByteUtil.decimal2fitHex(500, 4)); // Money consumed
+        data.append(ByteUtil.decimal2fitHex(300, 4)); // Max power
+        data.append(ByteUtil.decimal2fitHex(0, 2)); // Extra data
+        data.append(ByteUtil.decimal2fitHex(0, 2)); // Charge scheme
+
+        SendMessageForm sendForm = new SendMessageForm();
+
+        sendForm.setDeviceId(DEVICE_ID);
+        sendForm.setSubDeviceId(null);
+        sendForm.setSerialNo(3);
+        sendForm.setCmd(CMD.REC_0x11_ORDER_END_REPORT);
+        sendForm.setData(data.toString());
+
+        String actual = BaseProtocol.enCode(sendForm);
+
+        assertEquals(GOLDEN_FRAME_REC_0x11_ORDER_END, actual);
+    }
+
+    @Test
+    public void encodeSnd0x02LoginRepProducesGoldenFrame() {
+        Snd0x02EncodeForm cmd = new Snd0x02EncodeForm(
+            DEVICE_ID, // deviceId
+            null, // mainDeviceId
+            1, // serialNo
+            0 // result
+        );
+
+        /*
+        Timestamp must be mutated according to the golden frame
+        because the constructor initialises the timestamp to the
+        System.currentTimeMillis() / 1000.
+        */
+        cmd.setTimestamp(1782879012);
+
+        SendMessageForm sendForm = new SendMessageForm(cmd);
+
+        String actual = BaseProtocol.enCode(sendForm);
+
+        assertEquals(GOLDEN_FRAME_SND_0X02_LOGIN_REP, actual);
     }
 
     @Test
